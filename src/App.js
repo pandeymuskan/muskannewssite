64971a69b29553a317671be24415034880f79d2f
Muskan Pandey <bit19it56@bit.ac.in>
2023-07-08 08:20:13 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import SearchForm from './components/SearchForm'
+import Stories from './components/Stories'
+import Buttons from './components/Buttons'
+function App() {
+  return (
+    <>
+      <SearchForm />
+      <Buttons />
+      <Stories />
+    </>
+  )
+}
+
+export default App
